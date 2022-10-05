f628e24b25ba90322b4406fcdc1d6a1310bc66d4
Philip <philipesclamado@uvic.ca>
2022-10-05 11:52:57 -0700

    feat: header container

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header() {
-  return <div className="box h-20 w-auto p-4 border-4">Header</div>;
+  return <div className="box h-Header w-auto p-4 border-4">Header</div>;
 }
 
 export default Header;
