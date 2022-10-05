525691f98803a8304341004bb7e9290d7fed7201
Philip <philipesclamado@uvic.ca>
2022-10-05 11:54:11 -0700

    fix: module importing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import "../../styles/globals.css";
+import AwardsContainer from "../common/awards/container/AwardsContainer";
+import Footer from "../common/components/footer/Footer";
 import Header from "../common/components/header/Header";
+import GoalContainer from "../common/goal/container/GoalContainer";
 import MainContainer from "../common/main/container/MainContainer";
+import TeamContainer from "../common/team/container/TeamContainer";
 
 function App() {
   return (
     <div>
       <Header />
       <MainContainer />
+      <GoalContainer />
+      <TeamContainer />
+      <AwardsContainer />
+      <Footer />
     </div>
   );
 }
