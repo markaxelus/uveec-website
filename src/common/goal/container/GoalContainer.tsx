e764e0657fd99fc06d0c423dd5736d0fca67361d
Philip <philipesclamado@uvic.ca>
2022-10-05 11:53:12 -0700

    feat: goal container

---
+++ src/common/goal/container/GoalContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GoalContainer() {
+  return (
+    <div className="box h-GoalContainer w-auto p-4 border-4">
+      Goal Container
+    </div>
+  );
+}
+
+export default GoalContainer;
