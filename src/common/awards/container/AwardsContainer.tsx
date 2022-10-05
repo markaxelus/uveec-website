6d359bf09009f6a1ee7dcc863e06c297b9247263
Philip <philipesclamado@uvic.ca>
2022-10-05 11:51:50 -0700

    feat: awards container

---
+++ src/common/awards/container/AwardsContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AwardsContainer() {
+  return (
+    <div className="box h-AwardsContainer w-auto p-4 border-4">
+      Awards Container
+    </div>
+  );
+}
+
+export default AwardsContainer;
