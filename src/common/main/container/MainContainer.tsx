4683930843158c2161c59c9219715044413f897b
Philip <philipesclamado@uvic.ca>
2022-10-05 01:22:28 -0700

    feat: main container

---
+++ src/common/main/container/MainContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SponsorSlider from "../components/SponsorSlider/SponsorSlider";
+
+function MainContainer() {
+  return (
+    <>
+      <div className="box h-128 w-auto p-4 border-4">Main</div>
+      <SponsorSlider />
+    </>
+  );
+}
+
+export default MainContainer;
