1b9bdb62bf65f7ebe815b1785d78d91b62660fbb
Philip <philipesclamado@uvic.ca>
2022-10-05 11:53:32 -0700

    feat: team container

---
+++ src/common/team/container/TeamContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TeamContainer() {
+  return (
+    <div className="box h-TeamContainer w-auto p-4 border-4">
+      Team Container
+    </div>
+  );
+}
+
+export default TeamContainer;
