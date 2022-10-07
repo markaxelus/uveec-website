0b1251014e66c0a938611eadbad3e9343992a204
Philip <philipesclamado@uvic.ca>
2022-10-06 17:12:39 -0700

    feat: sponsor carousel container

---
+++ src/common/main/components/SponsorCarousel/SponsorCarousel.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SponsorCarousel() {
+  return (
+    <div className="box h-SponsorSlideshow w-auto p-4 border-4">Sponsor</div>
+  );
+}
+
+export default SponsorCarousel;
--- src/common/main/container/MainContainer.tsx
+++ src/common/main/container/MainContainer.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import SponsorSlideshow from "../components/SponsorSlideshow/SponsorSlideshow";
+import SponsorCarousel from "../components/SponsorCarousel/SponsorCarousel";
 
 function MainContainer() {
   return (
     <>
       <div className="box h-Main w-auto p-4 border-4">Main</div>
-      <SponsorSlideshow />
+      <SponsorCarousel />
     </>
   );
 }
