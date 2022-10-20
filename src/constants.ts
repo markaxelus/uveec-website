0281a2225e1eb7491fe9c1142d07c889f5b0995c
Philip <philipesclamado@uvic.ca>
2022-10-19 18:53:43 -0700

    chore: add Lorem ipsum

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,13 @@
 export const CompanyName =
   "University of Victoria Environmental Engineering Club";
+
+const short_lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dictum posuere nibh id vestibulum.";
+
+const medium_lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus aliquet accumsan. Sed ac tellus vel purus condimentum vestibulum. Nunc sodales leo eget diam consectetur, vel eleifend turpis imperdiet. Cras mattis fringilla euismod.";
+
+export const OurGoalsParagraphs = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum, tortor vitae accumsan elementum, lorem augue molestie sem, eu pretium lacus urna et augue. ",
+  "Praesent porta nunc in lorem iaculis aliquam. Sed eu maximus enim. Donec tempus neque sit amet lectus consectetur, ut sagittis tortor congue. Quisque ultrices a elit varius posuere. Mauris accumsan enim odio, eu dictum felis porttitor non. Aenean feugiat ligula id imperdiet ullamcorper. Suspendisse nec eros vitae arcu suscipit maximus. Ut pretium feugiat eleifend. Etiam ut erat placerat, posuere nisi non, sodales risus.",
+];
