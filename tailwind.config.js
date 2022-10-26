7d82e8fbde8364d401f720f43417938b84053c72
Philip <philipesclamado@uvic.ca>
2022-10-26 14:25:49 -0700

    chore: update colour util

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         royalBlue: "#17335B",
         lightBlue: "#3FA3DA",
         orange: "#FCB814",
-        red: "E92B2C",
+        red: "#E92B2C",
       },
     },
   },
