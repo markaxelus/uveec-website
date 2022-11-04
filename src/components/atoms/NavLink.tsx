882686ca775f8e2ddd5e383ab7984cc25e3931f4
Philip <philipesclamado@uvic.ca>
2022-11-03 19:34:35 -0700

    feat: header

--- src/components/atoms/NavLink.tsx
+++ src/components/atoms/NavLink.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import classNames from "classnames";
+import { useRouter } from "next/router";
+import React, { useMemo } from "react";
 
 interface NavLinkProps {
   link: string;
   name: string;
 }
 
-export const NavLink = () => {
-  return <div>NavLink</div>;
+export const NavLink = ({ link, name }: NavLinkProps) => {
+  const { asPath } = useRouter();
+
+  const isHighlighted = useMemo(() => {
+    if (asPath === "/" && name === "Home") return true;
+    return asPath.replace("/", "") === name.replace("/", "").toLowerCase();
+  }, [asPath, name]);
+
+  return (
+    <a
+      href={link}
+      className={classNames(
+        isHighlighted ? "text-orange" : "text-offwhite",
+        "text-lg"
+      )}
+    >
+      {name}
+    </a>
+  );
 };
--- src/components/molecules/NavBar.tsx
+++ src/components/molecules/NavBar.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { NavLink } from "../atoms";
 
 export const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="z-50 flex flex-row items-center space-x-4">
+      <NavLink name="Home" link="/" />
+      <NavLink name="About" link="/about" />
+      <NavLink name="Projects" link="/projects" />
+      <NavLink name="Sponsors" link="/sponsors" />
+      <NavLink name="Contact" link="/contact" />
+    </div>
+  );
 };
