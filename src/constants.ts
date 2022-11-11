e563ebb0da6f0ef2b46a3ce6de4a5eac2e85f0cc
Steve <steveanil2003@gmail.com>
2022-11-10 21:25:27 -0800

    feat: Sponsor Page

--- src/constants.ts
+++ src/constants.ts
@@ -11,3 +11,78 @@ export const OurGoalsParagraphs = [
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum, tortor vitae accumsan elementum, lorem augue molestie sem, eu pretium lacus urna et augue. ",
   "Praesent porta nunc in lorem iaculis aliquam. Sed eu maximus enim. Donec tempus neque sit amet lectus consectetur, ut sagittis tortor congue. Quisque ultrices a elit varius posuere. Mauris accumsan enim odio, eu dictum felis porttitor non. Aenean feugiat ligula id imperdiet ullamcorper. Suspendisse nec eros vitae arcu suscipit maximus. Ut pretium feugiat eleifend. Etiam ut erat placerat, posuere nisi non, sodales risus.",
 ];
+
+export const Contacts = [
+    {
+        img: "/images/social-media.png",
+    },
+    {
+        img: "/images/conversation.png"
+    },
+    {
+        img: "/images/message.png"
+    }
+]
+
+export const SponsorMap = [
+    {
+        img:"/images/altech.png",
+        title: "Altech",
+        link: "https://www.altechanodizing.com/"
+    },
+    {
+        img:"/images/altium.png",
+        title: "Altium",
+        link: "https://www.altium.com/"
+    },
+    {
+        img:"/images/bluerobotics.png",
+        title: "BlueRobotics",
+        link: "https://bluerobotics.com/"
+    },
+    {
+        img:"/images/digikey.png",
+        title: "DigiKey",
+        link: "https://www.digikey.ca/"
+    },
+    {
+        img:"/images/ess.png",
+        title: "Engineering Student Society",
+        link: "https://onlineacademiccommunity.uvic.ca/ess/"
+    },
+    {
+        img:"/images/fibertek.png",
+        title: "FiberTek",
+        link: "https://www.fibertek.com/"
+    },
+    {
+        img:"/images/maximumprototyping.png",
+        title: "Maximum Prototyping",
+        link: "https://maxproto.com/"
+    },
+    {
+        img:"/images/pcbway.png",
+        title: "PCBWay",
+        link: "https://www.pcbway.com/"
+    },
+    {
+        img:"/images/rainhouse.png",
+        title: "RainHouse",
+        link: "https://www.rainhouse.com/"
+    },
+    {
+        img:"/images/rapidharness.png",
+        title: "RapidHarness",
+        link: "https://rapidharness.com/"
+    },
+    {
+        img:"/images/teck.png",
+        title: "Teck",
+        link: "https://www.teck.com/"
+    },
+    {
+        img:"/images/uvic.png",
+        title: "University of Victoria",
+        link: "https://www.uvic.ca/"
+    },
+]
