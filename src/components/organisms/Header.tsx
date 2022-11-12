595552a23d2db4cd6d7f36d4d712bf4ce5ad39c0
Philip <philipesclamado@uvic.ca>
2022-11-11 17:53:25 -0800

    fix: logo width

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,7 +4,7 @@ import { NavBar } from "../molecules";
 export const Header = () => {
   return (
     <div className="sticky top-0 z-50 flex flex-row items-center justify-between h-16 w-full px-4 py-2 shadow-2xl bg-royalBlue">
-      <Logo src="uveec.png" className="h-10 w-32" />
+      <Logo src="uveec.png" className="h-10 w-28" />
       <div className="hidden md:block">
         <NavBar />
       </div>
