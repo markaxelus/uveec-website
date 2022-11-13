ac5d4d00ea1ad51a1bc0079ba93eda09954347ab
Philip <philipesclamado@uvic.ca>
2022-11-12 17:29:00 -0800

    feat: new about page template

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,258 +6,196 @@ import Image from "next/image";
 const About: NextPage = () => {
   return (
     <BaseLayout>
-      <div className="relative h-72 w-full">
-        <Image
-          layout="fill"
-          src="/images/header3.jpg"
-          className="object-cover blur-sm"
-        />
-        <div className="absolute bg-black h-full w-full opacity-50" />
+      <section id="about">
+        <div className="relative h-72 md:h-52 w-full">
+          <Image
+            layout="fill"
+            src="/images/header3.jpg"
+            className="object-cover blur-sm"
+          />
+          <div className="absolute bg-black h-full w-full opacity-50" />
 
-        <div className="relative text-lg text-white max-w-3xl mx-auto py-14 md:py-24 px-5 md:p-0">
-          {medium_lorem}
-        </div>
-      </div>
-
-      <section id="admin">
-        <div className="relative mx-auto my-28 w-96 md:w-[800px]">
-          <div className="flex flex-col md:flex-row">
-            <div className="text-center md:text-start flex flex-col justify-evenly">
-              <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight px-8">
-                Administration
-              </h2>
-              <p className="text-slate-400 px-8 mt-4 max-w-3xl space-y-6">
-                {medium_lorem}
-              </p>
-              <div className="flex flex-col md:flex-row">
-                <div className="flex flex-row justify-around mx-auto mt-4 h-auto w-52">
-                  <div className="relative h-14 w-14">
-                    <Image layout="fill" src="/images/user.png" />
-                  </div>
-                  <div>
-                    <div className="text-slate-900 font-semibold dark:text-white">
-                      Julia Jungwirth
-                    </div>
-                    <div className="text-slate-400 mt-0.5 text-sm leading-6">
-                      Team Lead
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-row justify-around mx-auto mt-4 h-auto w-52">
-                  <div className="relative h-14 w-14">
-                    <Image layout="fill" src="/images/user.png" />
-                  </div>
-                  <div>
-                    <div className="text-slate-900 font-semibold dark:text-white">
-                      Nathan Archibald
-                    </div>
-                    <div className="text-slate-400 mt-0.5 text-sm leading-6">
-                      Project Manager
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="mt-4 md:mt-0 relative h-[400px] w-full md:w-[900px]">
-              <Image
-                layout="fill"
-                src="/images/manufacturing.jpg"
-                className="object-cover rounded-none md:rounded-xl"
-              />
-            </div>
+          <div className="relative text-lg text-white max-w-3xl mx-auto py-10 md:py-16 px-5 md:p-0">
+            {medium_lorem}
           </div>
         </div>
       </section>
 
-      <section id="special project">
-        <div className="relative mx-auto my-28 w-96 md:w-[800px]">
-          <div className="flex flex-col md:flex-row">
-            <div className="text-center md:text-start flex flex-col justify-evenly">
-              <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight px-8">
-                Special Project
-              </h2>
-              <p className="text-slate-400 px-8 mt-4 max-w-3xl space-y-6">
-                {medium_lorem}
-              </p>
-              <div className="flex flex-row justify-around mx-auto mt-4 h-auto w-52">
-                <div className="relative h-14 w-14">
-                  <Image layout="fill" src="/images/user.png" />
-                </div>
-                <div>
-                  <div className="text-slate-900 font-semibold dark:text-white">
-                    Xavier Agustines
+      <main id="team">
+        <div className="pt-20 mb-20 space-y-20 overflow-hidden sm:pt-32 sm:mb-32 sm:space-y-32 md:pt-40 md:mb-40 md:space-y-40">
+          <section id="admin">
+            <div className="flex flex-col md:flex-row justify-start md:justify-evenly max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+              <div className="flex flex-col">
+                <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight">
+                  Administration
+                </h2>
+                <p className="text-slate-400 mt-4 max-w-3xl space-y-6">
+                  {medium_lorem}
+                </p>
+              </div>
+              <div className="flex flex-col pt-4">
+                <div className="h-52 w-52 bg-slate-400 rounded-xl"></div>
+                <div className="text-start md:text-center">
+                  <div className="text-slate-900 font-semibold">
+                    Julia Jungwirth
                   </div>
                   <div className="text-slate-400 mt-0.5 text-sm leading-6">
-                    Special Project Lead
+                    Team Lead
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col pt-4">
+                <div className="h-52 w-52 bg-slate-400 rounded-xl"></div>
+                <div className="text-start md:text-center">
+                  <div className="text-slate-900 font-semibold">
+                    Nathan Archibald
+                  </div>
+                  <div className="text-slate-400 mt-0.5 text-sm leading-6">
+                    Project Manager
                   </div>
                 </div>
               </div>
             </div>
-            <div className="mt-4 md:mt-0 relative h-[400px] w-full md:w-[900px] overflow-hidden">
-              <Image
-                layout="fill"
-                src="/images/manufacturing.jpg"
-                className="object-cover rounded-none md:rounded-xl"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+          </section>
 
-      <section id="mechanical">
-        <div className="relative mx-auto my-28 w-96 md:w-[800px]">
-          <div className="flex flex-col md:flex-row">
-            <div className="text-center md:text-start flex flex-col justify-evenly">
-              <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight px-8">
-                Mechanical
-              </h2>
-              <p className="text-slate-400 px-8 mt-4 max-w-3xl space-y-6">
-                {medium_lorem}
-              </p>
-              <div className="flex flex-col md:flex-row">
-                <div className="flex flex-row justify-around mx-auto mt-4 h-auto w-52">
-                  <div className="relative h-14 w-14">
-                    <Image layout="fill" src="/images/user.png" />
-                  </div>
-                  <div>
-                    <div className="text-slate-900 font-semibold dark:text-white">
-                      Mariana Latta Suazo
+          <section id="special project">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 flex flex-col md:flex-row">
+              <div className="flex flex-col">
+                <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight">
+                  Special Project
+                </h2>
+                <p className="text-slate-400 mt-4 max-w-3xl space-y-6">
+                  {medium_lorem}
+                </p>
+              </div>
+              <div className="flex flex-row pt-4">
+                <div className="flex flex-col">
+                  <div className="h-52 w-52 bg-slate-400 rounded-xl"></div>
+                  <div className="text-start md:text-center">
+                    <div className="text-slate-900 font-semibold">
+                      Xavier Agustines
                     </div>
                     <div className="text-slate-400 mt-0.5 text-sm leading-6">
-                      Mechanical Lead
+                      Special Project Lead
                     </div>
                   </div>
                 </div>
-                <div className="flex flex-row justify-around mx-auto mt-4 h-auto w-52">
-                  <div className="relative h-14 w-14">
-                    <Image layout="fill" src="/images/user.png" />
+              </div>
+            </div>
+          </section>
+
+          <section id="mechanical">
+            <div className="flex flex-col md:flex-row justify-start md:justify-evenly max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+              <div className="flex flex-col">
+                <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight">
+                  Mechanical
+                </h2>
+                <p className="text-slate-400 mt-4 max-w-3xl space-y-6">
+                  {medium_lorem}
+                </p>
+              </div>
+              <div className="flex flex-col pt-4">
+                <div className="h-52 w-52 bg-slate-400 rounded-xl"></div>
+                <div className="text-start md:text-center">
+                  <div className="text-slate-900 font-semibold">
+                    Mariana Latta Suazo
                   </div>
-                  <div>
-                    <div className="text-slate-900 font-semibold dark:text-white">
-                      Eli Grant
+                  <div className="text-slate-400 mt-0.5 text-sm leading-6">
+                    Mechanical Lead
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col pt-4">
+                <div className="h-52 w-52 bg-slate-400 rounded-xl"></div>
+                <div className="text-start md:text-center">
+                  <div className="text-slate-900 font-semibold">Eli Grant</div>
+                  <div className="text-slate-400 mt-0.5 text-sm leading-6">
+                    Co-Lead
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="electrical">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 flex flex-col md:flex-row">
+              <div className="flex flex-col">
+                <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight">
+                  Electrical
+                </h2>
+                <p className="text-slate-400 mt-4 max-w-3xl space-y-6">
+                  {medium_lorem}
+                </p>
+              </div>
+              <div className="flex flex-row pt-4">
+                <div className="flex flex-col">
+                  <div className="h-52 w-52 bg-slate-400 rounded-xl"></div>
+                  <div className="text-start md:text-center">
+                    <div className="text-slate-900 font-semibold">
+                      Michael Nicolaisen
                     </div>
                     <div className="text-slate-400 mt-0.5 text-sm leading-6">
-                      Mechanical Co-Lead
+                      Electrical Lead
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="mt-4 md:mt-0 relative h-[400px] w-full md:w-[900px]">
-              <Image
-                layout="fill"
-                src="/images/manufacturing.jpg"
-                className="object-cover rounded-none md:rounded-xl"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+          </section>
 
-      <section id="electrical">
-        <div className="relative mx-auto my-28 w-96 md:w-[800px]">
-          <div className="flex flex-col md:flex-row">
-            <div className="text-center md:text-start flex flex-col justify-evenly">
-              <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight px-8">
-                Electrical
-              </h2>
-              <p className="text-slate-400 px-8 mt-4 max-w-3xl space-y-6">
-                {medium_lorem}
-              </p>
-              <div className="flex flex-row justify-around mx-auto mt-4 h-auto w-52">
-                <div className="relative h-14 w-14">
-                  <Image layout="fill" src="/images/user.png" />
-                </div>
-                <div>
-                  <div className="text-slate-900 font-semibold dark:text-white">
-                    Michael Nicolaisen
-                  </div>
-                  <div className="text-slate-400 mt-0.5 text-sm leading-6">
-                    Electrical Lead
+          <section id="software">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 flex flex-col md:flex-row">
+              <div className="flex flex-col">
+                <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight">
+                  Software
+                </h2>
+                <p className="text-slate-400 mt-4 max-w-3xl space-y-6">
+                  {medium_lorem}
+                </p>
+              </div>
+              <div className="flex flex-row pt-4">
+                <div className="flex flex-col">
+                  <div className="h-52 w-52 bg-slate-400 rounded-xl"></div>
+                  <div className="text-start md:text-center">
+                    <div className="text-slate-900 font-semibold">
+                      Philip Esclamado
+                    </div>
+                    <div className="text-slate-400 mt-0.5 text-sm leading-6">
+                      Software Lead
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="mt-4 md:mt-0 relative h-[400px] w-full md:w-[900px] overflow-hidden">
-              <Image
-                layout="fill"
-                src="/images/manufacturing.jpg"
-                className="object-cover rounded-none md:rounded-xl"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+          </section>
 
-      <section id="software">
-        <div className="relative mx-auto my-28 w-96 md:w-[800px]">
-          <div className="flex flex-col md:flex-row">
-            <div className="text-center md:text-start flex flex-col justify-evenly">
-              <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight px-8">
-                Software
-              </h2>
-              <p className="text-slate-400 px-8 mt-4 max-w-3xl space-y-6">
-                {medium_lorem}
-              </p>
-              <div className="flex flex-row justify-around mx-auto mt-4 h-auto w-52">
-                <div className="relative h-14 w-14">
-                  <Image layout="fill" src="/images/user.png" />
-                </div>
-                <div>
-                  <div className="text-slate-900 font-semibold dark:text-white">
-                    Philip Esclamado
-                  </div>
-                  <div className="text-slate-400 mt-0.5 text-sm leading-6">
-                    Software Lead
+          <section id="advisor">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 flex flex-col md:flex-row">
+              <div className="flex flex-col">
+                <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight">
+                  Advisor
+                </h2>
+                <p className="text-slate-400 mt-4 max-w-3xl space-y-6">
+                  {medium_lorem}
+                </p>
+              </div>
+              <div className="flex flex-row pt-4">
+                <div className="flex flex-col">
+                  <div className="h-52 w-52 bg-slate-400 rounded-xl"></div>
+                  <div className="text-start md:text-center">
+                    <div className="text-slate-900 font-semibold">
+                      Dr. Valeo
+                    </div>
+                    <div className="text-slate-400 mt-0.5 text-sm leading-6">
+                      Faculty Supervisor
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="mt-4 md:mt-0 relative h-[400px] w-full md:w-[900px] overflow-hidden">
-              <Image
-                layout="fill"
-                src="/images/manufacturing.jpg"
-                className="object-cover rounded-none md:rounded-xl"
-              />
-            </div>
-          </div>
+          </section>
         </div>
-      </section>
-
-      <section id="advisor">
-        <div className="relative mx-auto my-28 w-96 md:w-[800px]">
-          <div className="flex flex-col md:flex-row">
-            <div className="text-center md:text-start flex flex-col justify-evenly">
-              <h2 className="mt-4 text-3xl sm:text-4xl text-slate-900 font-extrabold tracking-tight px-8">
-                Advisor
-              </h2>
-              <p className="text-slate-400 px-8 mt-4 max-w-3xl space-y-6">
-                {medium_lorem}
-              </p>
-              <div className="flex flex-row justify-around mx-auto mt-4 h-auto w-52">
-                <div className="relative h-14 w-14">
-                  <Image layout="fill" src="/images/user.png" />
-                </div>
-                <div>
-                  <div className="text-slate-900 font-semibold dark:text-white">
-                    Dr. Valeo
-                  </div>
-                  <div className="text-slate-400 mt-0.5 text-sm leading-6">
-                    Faculty Supervisor
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="mt-4 md:mt-0 relative h-[400px] w-full md:w-[900px] overflow-hidden">
-              <Image
-                layout="fill"
-                src="/images/manufacturing.jpg"
-                className="object-cover rounded-none md:rounded-xl"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+      </main>
     </BaseLayout>
   );
 };
