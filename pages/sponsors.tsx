4e3ab2917fc03bbf9e7822120d36a23c9d9ded54
Philip <philipesclamado@uvic.ca>
2022-11-14 10:46:10 -0800

    Auto stash before merge of "main" and "origin/main"

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -4,30 +4,31 @@ import Image from "next/image";
 import { SponsorMap } from "../src/constants";
 import Link from "next/link";
 
-
 const Sponsors: NextPage = () => {
   return (
     <BaseLayout>
-        <div className="container mx-auto px-6 grid lg:grid-cols-3 justify-items-center">
-          {SponsorMap.map(card => (
-            <div className="w-full h-64 lg:h-64 lg:w-96 relative hover:cursor-pointer overflow-hidden group">
-              <Link href={card.link} passHref>
-                <a target="_blank" rel="noopener noreferrer">
-                <Image 
+      <div className="grid lg:grid-cols-3 justify-items-center mx-auto px-6">
+        {SponsorMap.map((card) => (
+          <div className="w-full h-64 lg:h-64 lg:w-96 relative hover:cursor-pointer overflow-hidden group">
+            <Link href={card.link} passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image
                   src={card.img}
                   alt={card.title}
                   layout="fill"
                   objectFit="contain"
                   className="group-hover:blur-md transition-all duration-100 ease-out"
                 />
-              <div className="inset-0 flex justify-center items-center absolute p-5 opacity-0 scale-110 bg-black/40 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200 ease-out">
-                <h3 className="text-white text-2xl md:text-3xl font-bold">{card.title}</h3>
-              </div>
-                </a>
-              </Link>
-            </div>
-          ))}
-        </div>
+                <div className="inset-0 flex justify-center items-center absolute p-5 opacity-0 scale-110 bg-black/40 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200 ease-out">
+                  <h3 className="text-white text-2xl md:text-3xl font-bold">
+                    {card.title}
+                  </h3>
+                </div>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </BaseLayout>
   );
 };
