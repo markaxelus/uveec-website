d32a63a8c391fa05d0ded03551e0ad8fa0ce14ed
Philip <philipesclamado@uvic.ca>
2022-11-14 10:56:40 -0800

    fix: naming convention

--- src/constants.ts
+++ src/constants.ts
@@ -12,89 +12,89 @@ export const OurGoalsParagraphs = [
   "Praesent porta nunc in lorem iaculis aliquam. Sed eu maximus enim. Donec tempus neque sit amet lectus consectetur, ut sagittis tortor congue. Quisque ultrices a elit varius posuere. Mauris accumsan enim odio, eu dictum felis porttitor non. Aenean feugiat ligula id imperdiet ullamcorper. Suspendisse nec eros vitae arcu suscipit maximus. Ut pretium feugiat eleifend. Etiam ut erat placerat, posuere nisi non, sodales risus.",
 ];
 
-export const Contacts = [
-    {
-        img: "/images/social-media.png",
-        title: "Instagram",
-        button: "Follow",
-        passage: "Follow UVEEC on Instagram for news & updates",
-        link: "https://www.instagram.com/uvicenvironment/?hl=en"
-    },
-    {
-        img: "/images/conversation.png",
-        title: "Slack",
-        button: "Join",
-        passage: "Join the discussion with other members.",
-        link: "https://uvicenvironment.slack.com"
-    },
-    {
-        img: "/images/message.png",
-        title: "Contact",
-        button: "Message",
-        passage: "Send us a private message to get in touch.",
-        link: "mailto:uvicenvironment@gmail.com"
-    }
-]
+export const ContactsMap = [
+  {
+    img: "/images/social-media.png",
+    title: "Instagram",
+    button: "Follow",
+    passage: "Follow UVEEC on Instagram for news & updates",
+    link: "https://www.instagram.com/uvicenvironment/?hl=en",
+  },
+  {
+    img: "/images/conversation.png",
+    title: "Slack",
+    button: "Join",
+    passage: "Join the discussion with other members.",
+    link: "https://uvicenvironment.slack.com",
+  },
+  {
+    img: "/images/message.png",
+    title: "Contact",
+    button: "Message",
+    passage: "Send us a private message to get in touch.",
+    link: "mailto:uvicenvironment@gmail.com",
+  },
+];
 
 export const SponsorMap = [
-    {
-        img:"/images/altech.png",
-        title: "Altech",
-        link: "https://www.altechanodizing.com/"
-    },
-    {
-        img:"/images/altium.png",
-        title: "Altium",
-        link: "https://www.altium.com/"
-    },
-    {
-        img:"/images/bluerobotics.png",
-        title: "BlueRobotics",
-        link: "https://bluerobotics.com/"
-    },
-    {
-        img:"/images/digikey.png",
-        title: "DigiKey",
-        link: "https://www.digikey.ca/"
-    },
-    {
-        img:"/images/ess.png",
-        title: "Engineering Student Society",
-        link: "https://onlineacademiccommunity.uvic.ca/ess/"
-    },
-    {
-        img:"/images/fibertek.png",
-        title: "FiberTek",
-        link: "https://www.fibertek.com/"
-    },
-    {
-        img:"/images/maximumprototyping.png",
-        title: "Maximum Prototyping",
-        link: "https://maxproto.com/"
-    },
-    {
-        img:"/images/pcbway.png",
-        title: "PCBWay",
-        link: "https://www.pcbway.com/"
-    },
-    {
-        img:"/images/rainhouse.png",
-        title: "RainHouse",
-        link: "https://www.rainhouse.com/"
-    },
-    {
-        img:"/images/rapidharness.png",
-        title: "RapidHarness",
-        link: "https://rapidharness.com/"
-    },
-    {
-        img:"/images/teck.png",
-        title: "Teck",
-        link: "https://www.teck.com/"
-    },
-    {
-        img:"/images/uvic.png",
-        title: "University of Victoria",
-        link: "https://www.uvic.ca/"
-    },
-]
+  {
+    img: "/images/altech.png",
+    title: "Altech",
+    link: "https://www.altechanodizing.com/",
+  },
+  {
+    img: "/images/altium.png",
+    title: "Altium",
+    link: "https://www.altium.com/",
+  },
+  {
+    img: "/images/bluerobotics.png",
+    title: "BlueRobotics",
+    link: "https://bluerobotics.com/",
+  },
+  {
+    img: "/images/digikey.png",
+    title: "DigiKey",
+    link: "https://www.digikey.ca/",
+  },
+  {
+    img: "/images/ess.png",
+    title: "Engineering Student Society",
+    link: "https://onlineacademiccommunity.uvic.ca/ess/",
+  },
+  {
+    img: "/images/fibertek.png",
+    title: "FiberTek",
+    link: "https://www.fibertek.com/",
+  },
+  {
+    img: "/images/maximumprototyping.png",
+    title: "Maximum Prototyping",
+    link: "https://maxproto.com/",
+  },
+  {
+    img: "/images/pcbway.png",
+    title: "PCBWay",
+    link: "https://www.pcbway.com/",
+  },
+  {
+    img: "/images/rainhouse.png",
+    title: "RainHouse",
+    link: "https://www.rainhouse.com/",
+  },
+  {
+    img: "/images/rapidharness.png",
+    title: "RapidHarness",
+    link: "https://rapidharness.com/",
+  },
+  {
+    img: "/images/teck.png",
+    title: "Teck",
+    link: "https://www.teck.com/",
+  },
+  {
+    img: "/images/uvic.png",
+    title: "University of Victoria",
+    link: "https://www.uvic.ca/",
+  },
+];
