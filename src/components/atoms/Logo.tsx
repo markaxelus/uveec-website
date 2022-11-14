2f644ff52cbe64b0c67fa6ef67b608c66069a856
Philip <philipesclamado@uvic.ca>
2022-11-14 11:39:30 -0800

    feat: priority load

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,23 +1,15 @@
 import Image from "next/image";
 import classNames from "classnames";
 
-type Size = "sm" | "lg" | "xl";
-
 interface BaseLogoProps {
   src: string;
-  size?: Size;
   className?: string;
 }
 
-export const Logo = ({ src, size = "sm", className }: BaseLogoProps) => {
-  let sizeStyle = "w-16 h-16";
-
-  if (size === "lg") sizeStyle = "w-24 h-24";
-  if (size === "xl") sizeStyle = "w-32 h-32";
-
+export const Logo = ({ src, className }: BaseLogoProps) => {
   return (
-    <div className={classNames(className, sizeStyle, "relative")}>
-      <Image layout="fill" src={`/images/${src}`} />
+    <div className={classNames(className, "relative")}>
+      <Image layout="fill" src={`/images/${src}`} priority />
     </div>
   );
 };
