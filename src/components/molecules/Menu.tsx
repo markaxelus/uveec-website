dd2d4e38b5aab5d3c40cfa9d8fc2f1a2cec7e7c3
Steve <steveanil2003@gmail.com>
2022-11-21 19:52:24 -0800

    feat: optimized navbar and sponsors page for mobile

---
+++ src/components/molecules/Menu.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import { NavLink } from "../atoms";
+
+import { BiMenu, BiX } from 'react-icons/bi'
+
+export const Menu = () => {
+    const [toggle, setToggle] = useState(false)
+  return (
+    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
+        <div className="max-w-[1240px] m-auto flex justify-between items-center p-4">
+            <div onClick={() => setToggle(!toggle)} className="block z-10 text-3xl absolute right-2 top-5 cursor-pointer md:hidden">
+            {toggle ? <BiX size={30} className="fill-white"/> : <BiMenu size={30} className="fill-white" /> }
+            </div>
+            <div>
+                <ul className={`flex flex-col items-center py-7 md:flex md:items-center absolute bg-[#102543] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease in ${toggle ? 'top-16' : 'top-[-490px]'}`}>
+                    <li className="p-3 text-4xl">
+                        <NavLink name="Home" link="/" />
+                    </li>
+                    <li className="p-3 text-4xl">
+                        <NavLink name="About" link="/about" />
+                    </li>
+                    <li className="p-3 text-4xl">
+                        <NavLink name="Projects" link="/projects" />
+                    </li>
+                    <li className="p-3 text-4xl">
+                        <NavLink name="Sponsors" link="/sponsors" />
+                    </li>
+                    <li className="p-3 text-4xl">
+                        <NavLink name="Contact" link="/contact" />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  );
+};
