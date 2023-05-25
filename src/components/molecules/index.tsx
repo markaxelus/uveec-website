f77d9d5ed26e4e7470283a2158e72e02946e1faf
Philip <philipesclamado@uvic.ca>
2023-05-25 09:40:18 -0600

    fix: Type error

--- src/components/molecules/index.tsx
+++ src/components/molecules/index.tsx
@@ -1,6 +1,5 @@
 export { NavBar } from "./NavBar";
 export { SponsorMarquee } from "./SponsorMarquee";
-export { IndexMarquee } from "./IndexMarquee";
 export { HeroBanner } from "./HeroBanner";
 export { Menu } from "./Menu";
 export { ProfileCard } from "./ProfileCard";
