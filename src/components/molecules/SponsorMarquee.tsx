3e6426071862a197973b6950b744c3a433737480
markaxelus <mrkaxelus@gmail.com>
2024-06-29 20:21:48 -0700

    fix: darkMode Sponsor Marquee

--- src/components/molecules/SponsorMarquee.tsx
+++ src/components/molecules/SponsorMarquee.tsx
@@ -1,18 +1,35 @@
 import Marquee from "react-fast-marquee";
 import { Logo } from "../atoms";
-import { SponsorMap } from "../../constants";
+import { SponsorMap, SponsorMapDark } from "../../constants";
+import React, { useState, useEffect} from 'react';
 
 export const SponsorMarquee = () => {
+  
+  // Check the state 
+  const [isDarkMode, setIsDarkmode] = useState(false);
+
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    setIsDarkmode(darkModeMediaQuery.matches);
+
+    const handleChange = (e: MediaQueryListEvent) => setIsDarkmode(e.matches);
+    darkModeMediaQuery.addEventListener('change', handleChange);
+
+    return() => darkModeMediaQuery.removeEventListener('change', handleChange);
+  }, []);
+
+  const sponsorData = isDarkMode ? SponsorMapDark : SponsorMap;
+
   return (
     <Marquee direction="right" speed={80} gradient={false}>
       <div className="flex flex-row items-center justify-evenly space-x-24 space-y-4 mr-24">
-        {SponsorMap.map((card, i) => (
+        {sponsorData.map((card, i) => (
           <div
             key={i}
           >
             <Logo
               src={card.img}
-              className="h-12 w-56 dark:hidden"
+              className="h-12 w-56"
               classNameImage="object-contain"
             />
           </div>
